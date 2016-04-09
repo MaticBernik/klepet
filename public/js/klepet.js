7a869cd139550807ce4edd7dfb0227fcb800d352
Matic  Bernik <mb2551@student.uni-lj.si>
2016-04-09 01:28:56 +0000

    Zaključek dela na IV. nalogi

--- public/js/klepet.js
+++ public/js/klepet.js
@@ -3,11 +3,21 @@ var Klepet = function(socket) {
 };
 
 Klepet.prototype.posljiSporocilo = function(kanal, besedilo) {
-  var sporocilo = {
-    kanal: kanal,
-    besedilo: besedilo
-  };
-  this.socket.emit('sporocilo', sporocilo);
+  if(kanal=="dregljaj"){
+    var posiljatelj = $('#kanal').text().split(" ")[0];
+    var sporocilo = {
+      kanal: kanal,
+      vzdevek: besedilo,
+      posiljatelj: posiljatelj
+    };
+    socket.emit('dregljaj',sporocilo);
+  }else{
+    var sporocilo = {
+     kanal: kanal,
+     besedilo: besedilo
+    };
+    this.socket.emit('sporocilo', sporocilo);
+  }  
 };
 
 Klepet.prototype.spremeniKanal = function(kanal) {
@@ -31,6 +41,27 @@ Klepet.prototype.procesirajUkaz = function(ukaz) {
       besede.shift();
       var vzdevek = besede.join(' ');
       this.socket.emit('vzdevekSpremembaZahteva', vzdevek);
+      break;
+    case 'dregljaj':
+      besede.shift();
+      var vzdevek = besede.join(' ');
+      var uporabniki=$('#seznam-uporabnikov div');
+      var napaka=true;
+      for (var i=0;i<uporabniki.length;i++){
+        if(uporabniki[i].innerHTML == vzdevek){
+          napaka=false;
+          break;
+        }
+      }
+      if (!vzdevek){
+        sporocilo = "Neznan ukaz"
+      }else if(!napaka){
+        sporocilo = "Dregljaj za "+vzdevek; 
+        this.posljiSporocilo('dregljaj',vzdevek);
+      }else{
+        //sporocilo = "Uporabnik ne obstaja!"  
+        sporocilo = "Neznan ukaz";
+      }
       break;
     case 'zasebno':
       besede.shift();
